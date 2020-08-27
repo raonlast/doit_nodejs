1e010d20e38323926cbde4f0bef8209e5fb6eac5
Raon <RAON@Raonui-MacBook.local>
2020-08-28 02:43:29 +0900

    passport path setting

--- server.js
+++ server.js
@@ -37,9 +37,6 @@ app.use(expressSession({
 
 
 
-var route_leader = require('./routes/route_loader');
-route_leader.init(app, express.Router());
-
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -152,6 +149,66 @@ passport.deserializeUser(function(user, done) {
 
     done(null, user);
 });
+
+
+var router = express.Router();
+var route_leader = require('./routes/route_loader');
+route_leader.init(app, express.Router());
+
+router.route('/').get(function(req, res) {
+    console.log('/ 패스 요청됨');
+    res.render('index.ejs');
+});
+
+app.get('/login', function(req, res) {
+    console.log('/login 패스 요청됨');
+    res.render('login.ejs', {message: req.flash('loginMessage')});
+});
+
+app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+app.get('/signup', function(req, res) {
+    console.log('/signup 패스 요청됨 ');
+    res.render('signup.ejs', {message: req.flash('signupMessage')});
+});
+
+app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+
+router.route('/profile').get(function(req, res) {
+    console.log('/profile 패스 요청됨');
+
+    console.log('req.user 객체의 값');
+    console.log(req.user);
+
+    if(!req.user) {
+        console.log('사용자 인증이 안 된 상태');
+        res.redirect('/');
+        return;
+    }
+
+    console.log('사용자 인증된 상태');
+    if(Array.isArray(req.user)) {
+        res.render('profile.ejs', {user: req.user[0]._doc});
+    } else {
+        res.render('profile.ejs', {user: req.user});
+    }
+});
+
+app.get('/logout', function(req, res) {
+    console.log('/logout 패스 요청됨');
+    req.logout();
+    res.redirect('/');
+})
+
+
 
 
 console.log('config.server_port : %d', config.server_port);
